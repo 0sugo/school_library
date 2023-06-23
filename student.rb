64a37ddbd34ce52e42cc0fe0ff0390200f605acc
0sugo <josugo26@gmail.com>
2023-06-23 20:26:42 +0300

    Correct constructors arguments

--- student.rb
+++ student.rb
@@ -3,9 +3,11 @@ require_relative 'person'
 class Student < Person
   attr_accessor :name, :classroom
 
-  def initialize(_classroom)
-    super()
-    @name = name
+  def initialize(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
+  end
+  
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
