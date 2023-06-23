43bb22da70c66307e9cd383f0843a33843abe8ee
0sugo <josugo26@gmail.com>
2023-06-23 23:17:43 +0300

    Remove unnecessary frozen strings

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'base_decorator'
 # define capitalize secorator
 class CapitalizeDecorator < BaseDecorator
--- classroom.rb
+++ classroom.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # define book
 class Classroom
   attr_accessor :label, :students
