a297433e9630166d05e9a1b370410ff589fbf505
0sugo <josugo26@gmail.com>
2023-06-26 20:31:06 +0300

    Refactor person.rb

--- person.rb
+++ person.rb
@@ -1,11 +1,7 @@
 require_relative 'nameable'
-require('./rental')
-# define person class
-class Person < Nameable
-  attr_accessor :name, :age, :rentals
-  attr_reader :id
 
-  def initialize(age = nil, name = 'unknown', parent_permission: true)
+class Person < Nameable
+  def initialize(age, name, parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
@@ -13,24 +9,24 @@ class Person < Nameable
     @parent_permission = parent_permission
     @rentals = []
   end
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
 
-  def add_book_rental(date, book)
-    Rental.new(date, book, self)
+  def can_use_service?
+    of_age? || @parent_permission
   end
 
   def correct_name
     @name
   end
 
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
   private
 
   def of_age?
-    @age.to_i >= 18
-  end
-
-  def can_use_services?
-    return true if of_age? || @parent_permission
-
-    false
+    age >= 18
   end
 end
