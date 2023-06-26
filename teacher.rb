86851420e62629adec225e423b398327e9f6d6c4
0sugo <josugo26@gmail.com>
2023-06-26 20:33:15 +0300

    refactor teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,19 @@
-require_relative 'person'
-# define teacher
-class Teacher < Person
-  attr_accessor :specialization
+require_relative 'decorator'
 
-  def initialize(specialization, age, name)
-    super(age, name)
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
+  end
+end
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(age, name, specialization, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
-  def can_use_services
+  def can_use_service?
     true
   end
 end
