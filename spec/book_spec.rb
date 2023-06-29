cd2060a5b9adde1484107119cf4504c955f6b7c6
Esther Udondian <eudondian@gmail.com>
2023-06-29 21:17:49 +0100

    Create book_spec.rb

---
+++ spec/book_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+class Rental
+  attr_accessor :date,:person, :book
+
+  def initialize(date, book, person)
+    @date = date
+    @person = person
+    @book = book
+  end
+end
+describe Book do
+  before :each do
+    @book_prot = Book.new("Kadamnasi", "Oloo")
+
+  end
+  context "When initialised" do
+    it"Will create a book object" do
+      expect(@book_prot).to be_an_instance_of (Book)
+
+    end
+    it "creates a rentals instance" do
+      supposed_rental = @book_prot.add_rental("Kiganjo","12-12-2023")
+      expect(supposed_rental).to be_an_instance_of (Rental)
+    end
+  end
+end
